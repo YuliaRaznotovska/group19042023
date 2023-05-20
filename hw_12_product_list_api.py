c02bfc01143ac1fee21cbea29fdc74dd57c23870
Raznotovska Iuliia <hullabalooblues@gmail.com>
2023-05-20 17:57:23 +0300

    Fixed code and name of the key in the table

--- hw_12_product_list_api.py
+++ hw_12_product_list_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://script.google.com/macros/s/AKfycbyVb6YIjJo2N7gXavnLAp614O9PQJtXyL56G7t_QchTh6yy90V9ygHW_FycOXERKGjESw/exec'
+url = 'https://script.google.com/macros/s/AKfycbwXfRX9uzPIqkrrxWXHckcUf1t8jlg3_-aHsOspOP93wyzjg289PYplJBMa8iCppaCwiA/exec'
 
 response = requests.get(url)
 
@@ -17,7 +17,7 @@ gluten_free_price_sum = 0
 
 for product in clean_product_data:
     product_price = product['product_price']
-    product_contains_gluten = product['is_gluten']
+    product_contains_gluten = product['contains_gluten']
     product_stock_number = product['product_stock']
     total_product_price = product_price * product_stock_number
     product_price_set.append(total_product_price)
