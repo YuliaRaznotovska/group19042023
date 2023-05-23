437d9e184b3d387b16e67747be6365d947cb35a1
Raznotovska Iuliia <hullabalooblues@gmail.com>
2023-05-23 17:02:48 +0300

    Fixed Homework - 13

--- homework_13.py
+++ homework_13.py
@@ -1,29 +1,28 @@
 import requests
 
-url = 'https://dummyjson.com/users'
-
-response = requests.get(url)
-
-dummyjson_data = response.json()
-
-clean_data = dummyjson_data['users']
+url = 'https://dummyjson.com/users/{id}'
 
 brown_hair_set = []
-brown_hair_set_average_age = 0
 louisville_citizens_set = []
 
-for user in clean_data:
-    if user['hair']['color'] == 'Brown':
-        brown_hair_user_age = user['age']
+for page in range(1, 101):
+    response = requests.get(url.format(id=page))
+    dummyjson_data = response.json()
+    if dummyjson_data['hair']['color'] == 'Brown':
+        brown_hair_user_age = dummyjson_data['age']
         brown_hair_set.append(brown_hair_user_age)
-        brown_hair_set_length = len(brown_hair_set)
-        brown_hair_set_sum = sum(brown_hair_set)
-        brown_hair_set_average_age = int(brown_hair_set_sum / brown_hair_set_length)
-    elif user['address']['city'] == 'Louisville':
-        louisville_citizen_first_name = user['firstName']
-        louisville_citizen_last_name = user['lastName']
-        louisville_citizen_full_name = f'{louisville_citizen_first_name} {louisville_citizen_last_name}'
-        louisville_citizens_set.append(louisville_citizen_full_name)
+    try:
+        if dummyjson_data['address']['city'] == 'Louisville':
+            louisville_citizen_first_name = dummyjson_data['firstName']
+            louisville_citizen_last_name = dummyjson_data['lastName']
+            louisville_citizen_full_name = f'{louisville_citizen_first_name} {louisville_citizen_last_name}'
+            louisville_citizens_set.append(louisville_citizen_full_name)
+    except KeyError:
+        pass
+
+brown_hair_set_length = len(brown_hair_set)
+brown_hair_set_sum = sum(brown_hair_set)
+brown_hair_set_average_age = int(brown_hair_set_sum / brown_hair_set_length)
 
 louisville_citizens = ', '.join(map(str, louisville_citizens_set))
 
